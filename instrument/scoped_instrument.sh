686c0d4c40a4a7025d3e37c5aa09dec83ec3af35
Wei Ning <nwcgnw@gmail.com>
2018-08-03 12:36:17 +1200

    - add missing source to instrument

--- instrument/scoped_instrument.sh
+++ instrument/scoped_instrument.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# source
+# NOTE: I haven't found a single article that explains this concept
+# well enough....
+# https://gcc.gnu.org/onlinedocs/gcc-6.3.0/gcc/Instrumentation-Options.html
+# https://gcc.gnu.org/onlinedocs/gcc-4.6.0/gcc/i386-and-x86_002d64-Options.html#i386-and-x86_002d64-Options
+# https://gcc.gnu.org/onlinedocs/gcc/x86-Function-Attributes.html#x86-Function-Attributes
+# https://gcc.gnu.org/onlinedocs/gcc/Attribute-Syntax.html
+# https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#Common-Function-Attributes
+# https://gcc.gnu.org/onlinedocs/gcc-5.2.0/gcc/Label-Attributes.html
+# https://stackoverflow.com/questions/44978546/is-it-possible-to-use-gcc-to-compile-one-section-of-a-code-file-with-specific-co
+# 
+
 generateSUT() {
     echo "
 __attribute__((no_instrument_function)) 
